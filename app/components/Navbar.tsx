8e61bcaaf3a12842c8430d1e5d2b51b3522e6986
Dave-devs <Odetundeoreoluwadavid@gmail.com>
2025-01-08 01:02:22 +0100

    fix (navbar): correct style break-point error

--- app/components/Navbar.tsx
+++ app/components/Navbar.tsx
@@ -1,10 +1,10 @@
 import Image from "next/image";
 import Link from "next/link";
-import React from "react";
+import { RiMenu3Fill } from "react-icons/ri";
 
 export default function Navbar() {
   return (
-    <nav className="hidden md:block fixed inset-x-0 w-[80%] mx-auto bg-opacity-70 bg-white p-2 my-4 rounded-full shadow-md z-50">
+    <nav className="fixed inset-x-0 top-0 w-[80%] mx-auto bg-white/70 px-4 py-2 my-4 rounded-full shadow-md z-50 block">
       <div className="flex justify-between items-center px-4 max-w-7xl mx-auto">
         {/* Logo */}
         <div className="flex items-center space-x-4">
@@ -15,20 +15,25 @@ export default function Navbar() {
             <h1 className="text-2xl font-bold text-black">Luxor</h1>
           </Link>
         </div>
+
         {/* Navigation */}
-        <ul className="flex justify-center space-x-8 text-black">
+        <ul className="hidden lg:flex justify-center space-x-8 text-black">
           <Link href={"#/"}></Link>
           <Link href={"#about"}>About</Link>
           <Link href={"#services"}>Services</Link>
           <Link href={"#gallery"}>Gallery</Link>
           <Link href={"#pricing"}>Pricing</Link>
         </ul>
+
         {/* Contact Button */}
         <Link href={"#contact"}>
           <button className="hidden md:block px-6 py-2 text-white bg-primary rounded-full">
             Contact Us
           </button>
         </Link>
+        
+        {/* Mobile Navigation Menu */}
+        <RiMenu3Fill className="md:hidden text-2xl cursor-pointer" />
       </div>
     </nav>
   );
